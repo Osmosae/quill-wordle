a8d43d04ffb8278a952fe8378a649efd4682768f
Seán Quill <osmosae@gmail.com>
2023-10-14 15:30:01 +0100

    changing props for potential keyboard UI

--- src/Components/Game/RenderGuesses.jsx
+++ src/Components/Game/RenderGuesses.jsx
@@ -1,15 +1,34 @@
 import Guess from "./Guess"
+// import KeyBoard from "../Keyboard/KeyBoard"
 import { range } from "../../helpers/utils"
+import { checkGuess } from "../../helpers/game-helpers"
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants/constants"
 
 import PropTypes from "prop-types"
 
 function RenderGuesses({ guesses, answer }) {
+    const letterStatusMap = guesses.map((guess) => checkGuess(guess, answer))
+
+    // function getStatusByLetter(onValidatedGuesses) {
+    //     const statusObj = {}
+
+    //     onValidatedGuesses.forEach((guess) => {
+    //         guess.forEach(({ letter, status }) => {
+    //             statusObj[letter] = status
+    //         })
+    //     })
+
+    //     return statusObj
+    // }
+
+    // const letterColorMap = getStatusByLetter(letterStatusMap)
+
     return (
         <div className="guess-results">
             {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
-                <Guess key={num} value={guesses[num]} answer={answer} />
+                <Guess key={num} value={letterStatusMap[num]} />
             ))}
+            {/* <KeyBoard letterColorMap={letterColorMap} /> */}
         </div>
     )
 }
