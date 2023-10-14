cdd2bc8c8d8f2a8946c191953515d709519f591a
Seán Quill <osmosae@gmail.com>
2023-10-14 15:25:28 +0100

    making keyboard more componentish

--- src/Components/Keyboard/KeyBoard.jsx
+++ src/Components/Keyboard/KeyBoard.jsx
@@ -1,32 +1,36 @@
-function KeyBoard() {
+import PropTypes from "prop-types"
+
+function Row({ level, rowArr }) {
+    return (
+        <div className={`${level}-row`}>
+            {rowArr.map((letter) => (
+                <span className="small-cell" key={letter}>
+                    {letter}
+                </span>
+            ))}
+        </div>
+    )
+}
+
+function KeyBoard({ letterColorMap }) {
     const topRow = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"]
     const midRow = ["a", "s", "d", "f", "g", "h", "j", "k", "l"]
     const botRow = ["z", "x", "c", "v", "b", "n", "m"]
     return (
         <div className="keyboard">
-            <div className="first-row">
-                {topRow.map((letter) => (
-                    <span className="small-cell" key={letter}>
-                        {letter}
-                    </span>
-                ))}
-            </div>
-            <div className="second-row">
-                {midRow.map((letter) => (
-                    <span className="small-cell" key={letter}>
-                        {letter}
-                    </span>
-                ))}
-            </div>
-            <div className="third-row">
-                {botRow.map((letter) => (
-                    <span className="small-cell" key={letter}>
-                        {letter}
-                    </span>
-                ))}
-            </div>
+            <Row level="first" rowArr={topRow} />
+            <Row level="second" rowArr={midRow} />
+            <Row level="third" rowArr={botRow} />
         </div>
     )
 }
 
 export default KeyBoard
+
+KeyBoard.propTypes = {
+    letterColorMap: PropTypes.object,
+}
+Row.propTypes = {
+    level: PropTypes.string,
+    rowArr: PropTypes.array,
+}
