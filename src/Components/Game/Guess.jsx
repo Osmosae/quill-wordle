c7b2801d1b05d71f94156bd4b37e2cee36190d4c
Seán Quill <osmosae@gmail.com>
2023-10-14 15:08:06 +0100

    change of value props to array

--- src/Components/Game/Guess.jsx
+++ src/Components/Game/Guess.jsx
@@ -1,4 +1,3 @@
-import { checkGuess } from "../../helpers/game-helpers"
 import { range } from "../../helpers/utils"
 
 import PropTypes from "prop-types"
@@ -7,12 +6,11 @@ function Cell({ letter, status }) {
     return <span className={status ? `${status} cell` : "cell"}>{letter}</span>
 }
 
-function Guess({ value, answer }) {
-    const result = checkGuess(value, answer)
+function Guess({ value }) {
     return (
         <p className="guess">
             {range(5).map((num) => (
-                <Cell letter={result ? result[num].letter : undefined} status={result ? result[num].status : undefined} key={num} />
+                <Cell letter={value ? value[num].letter : undefined} status={value ? value[num].status : undefined} key={num} />
             ))}
         </p>
     )
@@ -21,7 +19,7 @@ function Guess({ value, answer }) {
 export default Guess
 
 Guess.propTypes = {
-    value: PropTypes.string,
+    value: PropTypes.array,
     answer: PropTypes.string,
 }
 Cell.propTypes = {
