e8aac6dcacf317ea2f1c484f8a04956374c6ceb5
Seán Quill <osmosae@gmail.com>
2023-10-18 15:40:49 +0100

    focus input on intial load

--- src/Components/Game/UserInput.jsx
+++ src/Components/Game/UserInput.jsx
@@ -1,8 +1,14 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import PropTypes from "prop-types"
 
 function UserInput({ handleAddGuesses, gameStatus }) {
     const [enteredGuess, setEnteredGuess] = useState("")
+
+    const inputRef = useRef()
+
+    useEffect(() => {
+        inputRef.current.focus()
+    }, [])
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -13,7 +19,7 @@ function UserInput({ handleAddGuesses, gameStatus }) {
     return (
         <form className="guess-input-wrapper" onSubmit={handleSubmit}>
             {/* <label htmlFor="guess-input">Enter guess:</label> */}
-            <input required minLength={5} maxLength={5} pattern="[a-zA-Z]{5}" id="guess-input" type="text" value={enteredGuess} placeholder="Enter Guess" onChange={(e) => setEnteredGuess(e.target.value.toUpperCase())} disabled={gameStatus !== "running"} />
+            <input required minLength={5} maxLength={5} pattern="[a-zA-Z]{5}" id="guess-input" type="text" value={enteredGuess} placeholder="Enter Guess" onChange={(e) => setEnteredGuess(e.target.value.toUpperCase())} disabled={gameStatus !== "running"} ref={inputRef} />
         </form>
     )
 }
